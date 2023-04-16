05f598b16bdd73a915fea71e5195ee07372acf52
Jake Lawhorne <jake.lawhorne@gmail.com>
2023-04-15 20:31:18 -0400

    did stuff

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // Import stylesheets
-require("./style.css");
-require("node_modules/bootstrap/dist/css/bootstrap.min.css");
-const form = document.querySelector('#defineform');
-form.onsubmit = () => {
-    const formData = new FormData(form);
-    console.log(formData);
-    const text = formData.get('defineword');
-    console.log(text);
-    return false; // prevent reload
+// import './style.css';
+// import 'node_modules/bootstrap/dist/css/bootstrap.min.css'
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
+const form = document.querySelector('#defineform');
+const list = document.querySelector('.list-unstyled');
+const header = document.querySelector('h1');
+form.onsubmit = (event) => __awaiter(void 0, void 0, void 0, function* () {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const text = formData.get('defineword');
+    try {
+        const response = yield fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${text}`);
+        const data = yield response.json();
+        // update header
+        header.innerText = text;
+        // update list
+        list.innerHTML = '';
+        data[0].meanings.forEach((meaning) => {
+            const li = document.createElement('li');
+            li.innerText = `${meaning.partOfSpeech} - ${meaning.definitions[0].definition}`;
+            list.appendChild(li);
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,16 +1,33 @@
 // Import stylesheets
-import './style.css';
-import 'node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import './style.css';
+// import 'node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 
 const form: HTMLFormElement = document.querySelector('#defineform')!;
+const list: HTMLUListElement = document.querySelector('.list-unstyled')!;
+const header: HTMLHeadingElement = document.querySelector('h1')!;
 
+form.onsubmit = async (event) => {
+  event.preventDefault();
 
-form.onsubmit = () => {
   const formData = new FormData(form);
-
-  console.log(formData);
   const text = formData.get('defineword') as string;
-  console.log(text);
-  return false; // prevent reload
+
+  try {
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${text}`);
+    const data = await response.json();
+
+    // update header
+    header.innerText = text;
+
+    // update list
+    list.innerHTML = '';
+    data[0].meanings.forEach((meaning: any) => {
+      const li = document.createElement('li');
+      li.innerText = `${meaning.partOfSpeech} - ${meaning.definitions[0].definition}`;
+      list.appendChild(li);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
